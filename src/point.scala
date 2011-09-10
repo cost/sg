58fbe8e59f748be8a58a51f493710329908a44fa
Kuleshov Alexander <kuleshovmail@gmail.com>
2011-09-10 00:38:44 +0000

    point move and distance between 2 point functions added

--- src/point.scala
+++ src/point.scala
@@ -22,4 +22,16 @@ class Point {
     // y getter
     def y() =
 	this.yCoord
+	
+    //move point
+    def movePoint(newX : Int, newY : Int) = {
+	xCoord = newX
+	yCoord = newY
+    }
+    
+    // range between 2 point
+    def range(p1 : Point, p2 : Point) = {
+        var sqr = (x : Int) => x * x
+	sqrt(sqr.apply(p1.x - p2.x) - sqr.apply(p1.y - p2.y))
+    }
 }
